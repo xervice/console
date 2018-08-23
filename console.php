017b80d78c74fee6e918c6643530699706eb289d
Mike Bertram <contact@mibexx.de>
2018-08-23 11:19:06 +0200

    * Console fix

--- console.php
+++ console.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Xervice\Core\Business\Model\Locator\Locator;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/autoload.php';
 
 Locator::getInstance()->console()->facade()->runApplication();
